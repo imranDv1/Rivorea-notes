da755016e54a7ae34e747fc7ac024294dc573c27
Imran Dv <uffmink@gmail.com>
2025-10-13 16:52:51 +0300

    fix expo

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
-import { expo } from "@better-auth/expo";
 import { prisma } from "./db";
 
 // If your Prisma file is located elsewhere, you can change the path
@@ -22,8 +21,5 @@ export const auth = betterAuth({
     },
     },
 
-    baseURL : process.env.NEXT_PUBLIC_APP_URL,
-  plugins: [expo()],
- 
      
 });
