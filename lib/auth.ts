b242404b769f57360a2d63bd7ca6c5b5662b999b
Imran Dv <uffmink@gmail.com>
2025-10-15 17:23:39 +0300

    addd expo plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import { nextCookies } from "better-auth/next-js";
+import { expo } from "@better-auth/expo";
 import { prisma } from "./db";
 
 // If your Prisma file is located elsewhere, you can change the path
@@ -13,6 +14,12 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   }, 
+    plugins: [
+    expo({
+ 
+    }),
+    nextCookies(), // <-- last
+  ],
 
     socialProviders : {
        google: {
@@ -20,6 +27,8 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
     },
-baseURL : process.env.NEXT_PUBLIC_APP_URL
+baseURL : process.env.NEXT_PUBLIC_APP_URL,
+    trustedOrigins: ["myapp://", "http://localhost:3000", "https://rivorea-notes.vercel.app"],
+
      
 });
