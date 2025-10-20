fa20cc6608c1448d448503a952c4f9d87df03ab0
Imran Dv <uffmink@gmail.com>
2025-10-20 10:21:48 +0300

    add android id

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,29 +5,30 @@ import { expo } from "@better-auth/expo";
 import { prisma } from "./db";
 
 // If your Prisma file is located elsewhere, you can change the path
+const isMobile = typeof navigator !== "undefined" && navigator.userAgent.includes("Expo");
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "sqlite", // or "mysql", "postgresql", ...etc
+  }),
 
-    emailAndPassword: { 
-    enabled: true, 
-  }, 
-    plugins: [
-    expo({
- 
-    }),
-  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [expo({})],
 
-    socialProviders : {
-       google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
+  socialProviders: {
+    google: {
+        clientId: isMobile
+        ? process.env.GOOGLE_CLIENT_ID_ANDROID!
+        : process.env.GOOGLE_CLIENT_ID_WEB!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    },
-baseURL : process.env.NEXT_PUBLIC_APP_URL,
-    trustedOrigins: ["myapp://", "http://localhost:3000", "https://rivorea-notes.vercel.app"],
-
-     
+  },
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  trustedOrigins: [
+    "myapp://",
+    "http://localhost:3000",
+    "https://rivorea-notes.vercel.app",
+  ],
 });
