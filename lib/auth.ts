81bd82d19716d1a1732240a1fd5c59552c364300
Imran Dv <uffmink@gmail.com>
2025-10-20 18:38:00 +0300

    fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,8 +18,7 @@ export const auth = betterAuth({
 
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID_ANDROID!,
-  
+        clientId: process.env.GOOGLE_CLIENT_ID_WEB!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
